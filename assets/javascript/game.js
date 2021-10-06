42a435df33d0e7721e31d34146a0328b21523cf8
Brian Helms <bhelms2132@gmail.com>
2021-10-05 21:04:05 -0400

    letter array created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
 // Variables 
+var letters = ["K", "A", "R", "O", "N", "W", "P", "Y"];
 var wins = [];
 var losses = [];
 var guessesLeft = [];
 var yourGuesses = [];
 
-document.getElementById("wins").innerHTML = wins;
+/* document.getElementById("wins").innerHTML = wins;
 document.getElementById("losses").innerHTML = losses;
 document.getElementById("guessesLeft").innerHTML = guessesLeft;
-document.getElementById("yourGuesses").innerHTML = yourGuesses;
+document.getElementById("yourGuesses").innerHTML = yourGuesses;*/
+
