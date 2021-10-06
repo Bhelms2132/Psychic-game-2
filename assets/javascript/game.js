7ff3dfc54f7ae0bb7bf129b0e33f300badc4587a
Brian Helms <bhelms2132@gmail.com>
2021-10-05 20:43:41 -0400

    variables added to js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Variables 
+var wins = [];
+var losses = [];
+var guessesLeft = [];
+var yourGuesses = [];
+
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
+document.getElementById("yourGuesses").innerHTML = yourGuesses;
