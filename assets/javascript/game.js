8bfa7910dbcb06d0d455f396099321f9f4b3c406
Brian Helms <bhelms2132@gmail.com>
2021-10-07 20:36:58 -0400

    for loop created for computer guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,17 @@
 // Variables 
-var letters = ["K", "A", "R", "O", "N", "W", "P", "Y"];
-var wins = [];
-var losses = [];
-var guessesLeft = [];
-var yourGuesses = [];
+var compLetters = ["K", "A", "R", "O", "N", "W", "P", "Y"];
+var userWins = '';
+var userLosses = '';
+var userGuessesLeft = '';
+var yourGuesses = '';
 
-/* document.getElementById("wins").innerHTML = wins;
-document.getElementById("losses").innerHTML = losses;
-document.getElementById("guessesLeft").innerHTML = guessesLeft;
-document.getElementById("yourGuesses").innerHTML = yourGuesses;*/
+/*document.getElementById("wins").innerHTML = userWins;
+document.getElementById("losses").innerHTML = userLosses;
+document.getElementById("guessesLeft").innerHTML = userGuessesLeft;
+document.getElementById("yourGuesses").innerHTML = yourGuesses;
+document.getElementById("yourGuesses").innerHTML = compLetters;*/
 
+// for loop for Computer Guessed Letters 
+for (var i = 0; i < compLetters.length; i++) {
+    console.log(compLetters[i]);
+}
