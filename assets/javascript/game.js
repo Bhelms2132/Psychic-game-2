ae629d23fd5e94c8441564375ced1c507db59ce0
Brian Helms <bhelms2132@gmail.com>
2021-10-11 14:05:19 -0400

    Variable created and element added to html for computer choice's

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,25 @@
-// Variables 
-var compLetters = ["K", "A", "R", "O", "N", "W", "P", "Y"];
+// Computer Letters Array
+var computerLetters = ["K", "A", "R", "O", "N", "W", "P", "Y"];
+
+// Variables that hold references to the places in the HTML where we want to display things.
 var userWins = document.getElementById('wins');
 var userLosses = document.getElementById('losses');
 var guessesLeft = document.getElementById('guesses-left');
 var yourGuesses = document.getElementById('your-guesses');
+var computerChooseLetter = document.getElementById('computer-choice');
 
-
-
-// for loop for Computer Guessed Letters 
-for (var i = 0; i < compLetters.length; i++) {
-    var currentLetter = compLetters[i];
+/* for loop for Computer Guessed Letters 
+for (var i = 0; i < computerLetters.length; i++) {
+    var currentLetter = computerLetters[i];
 
     console.log(currentLetter);
+    document.onkeyup = function(event) {
+        userWins.textContent = event.key;
+    }
+    
+}*/
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
     
 }
