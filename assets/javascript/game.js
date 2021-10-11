99d8195a7fc82d3b1d043261f060c7fbc7713a0f
Brian Helms <bhelms2132@gmail.com>
2021-10-11 13:22:34 -0400

    Recreated variables in js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,16 @@
 // Variables 
 var compLetters = ["K", "A", "R", "O", "N", "W", "P", "Y"];
-var userWins = '';
-var userLosses = '';
-var userGuessesLeft = '';
-var yourGuesses = '';
+var userWins = document.getElementById('wins');
+var userLosses = document.getElementById('losses');
+var guessesLeft = document.getElementById('guesses-left');
+var yourGuesses = document.getElementById('your-guesses');
 
-/*document.getElementById("wins").innerHTML = userWins;
-document.getElementById("losses").innerHTML = userLosses;
-document.getElementById("guessesLeft").innerHTML = userGuessesLeft;
-document.getElementById("yourGuesses").innerHTML = yourGuesses;
-document.getElementById("yourGuesses").innerHTML = compLetters;*/
+
 
 // for loop for Computer Guessed Letters 
 for (var i = 0; i < compLetters.length; i++) {
     var currentLetter = compLetters[i];
 
     console.log(currentLetter);
+    
 }
